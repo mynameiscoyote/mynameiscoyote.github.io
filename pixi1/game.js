b6024ed193a30953f6bc173ea9f3bcf4fd748ef5
Jaela <mynameiscoyote@gmail.com>
2014-10-14 20:48:54 -0700

    adds blue alien, green planet, and disco sun. also resizes whole shebang.

--- pixi1/game.js
+++ pixi1/game.js
@@ -4,65 +4,147 @@
 var stage = new PIXI.Stage(0x070c56);
 
 // create a renderer instance.
-var renderer = PIXI.autoDetectRenderer(1200, 900);
+var renderer = PIXI.autoDetectRenderer(1400, 700);
+
+var graphics = new PIXI.Graphics();
+
+graphics.setInteractive(true);
+
+
 
 // add the renderer view element to the DOM
 document.body.appendChild(renderer.view);
 
 requestAnimFrame( animate );
 
-// create a texture from an image path
-var texture = PIXI.Texture.fromImage("images/alien5.png");
-// create a new Sprite using the texture
-var alien5 = new PIXI.Sprite(texture);
 
-// center sprite 1's anchor point
-alien5.anchor.x = 0.5;
-alien5.anchor.y = 0.5;
+// create a texture from an image path
+var texture = PIXI.Texture.fromImage("images/universe.jpg");
+// create a new Sprite using the texture
+var universe = new PIXI.Sprite(texture);
 
 // move the first sprite to the center of the screen
-alien5.position.x = 200;
-alien5.position.y = 150;
+universe.position.x = 0;
+universe.position.y = 0;
+
+//fit sprite to screen size
+universe.width = 1400;
+
 
 // create a texture from an image path
-var texture = PIXI.Texture.fromImage("images/alien.png");
+var texture = PIXI.Texture.fromImage("images/disco.gif");
 // create a new Sprite using the texture
-var alien = new PIXI.Sprite(texture);
+var disco = new PIXI.Sprite(texture);
 
-// center sprite 2's anchor point
-alien.anchor.x = 1.75;
-alien.anchor.y = 1.25;
+// move the first sprite to the center of the screen
+disco.position.x = 1100;
+disco.position.y = 700;
 
-// move the second sprite to the center of the screen
-alien.position.x = 400;
-alien.position.y = 150;
+disco.anchor.x = 0.5;
+disco.anchor.y = 0.5;
+
+disco.hitArea = new PIXI.Rectangle(0, 0, 1000, 1000);
 
 // create a texture from an image path
-var texture = PIXI.Texture.fromImage("images/greenplanet.png");
+var texture = PIXI.Texture.fromImage("images/planet.png");
 // create a new Sprite using the texture
 var planet = new PIXI.Sprite(texture);
 
 // center sprite 1's anchor point
-planet.anchor.x = 1.0;
-planet.anchor.y = 1.0;
+planet.anchor.x = 2.0;
+planet.anchor.y = 2.0;
 
-// move the first sprite to the center of the screen
-planet.position.x = 1500;
+// move the first sprite to desired position on the screen
+planet.position.x = 1300;
 planet.position.y = 1000;
 
 
-stage.addChild(alien5);
-stage.addChild(alien);
+// create a texture from an image path
+var texture = PIXI.Texture.fromImage("images/aliensmall.png");
+// create a new Sprite using the texture
+var aliensmall = new PIXI.Sprite(texture);
+
+// center sprite 2's anchor point
+aliensmall.anchor.x = 1.75;
+aliensmall.anchor.y = 1.25;
+
+// move the second sprite to the center of the screen
+aliensmall.position.x = 400;
+aliensmall.position.y = 150;
+
+
+// create a texture from an image path
+var texture = PIXI.Texture.fromImage("images/alienbig.png");
+// create a new Sprite using the texture
+var alienbig = new PIXI.Sprite(texture);
+
+// center sprite 1's anchor point
+alienbig.anchor.x = 0.5;
+alienbig.anchor.y = 0.5;
+
+// move the first sprite to the center of the screen
+alienbig.position.x = 200;
+alienbig.position.y = 150;
+
+alienbig.height = 200;
+alienbig.width = 150;
+
+
+// create a texture from an image path
+var texture = PIXI.Texture.fromImage("images/alienblue.png");
+// create a new Sprite using the texture
+var alienblue = new PIXI.Sprite(texture);
+
+// center sprite 1's anchor point
+alienblue.anchor.x = 0.5;
+alienblue.anchor.y = 0.5;
+
+// move the first sprite to the center of the screen
+alienblue.position.x = 300;
+alienblue.position.y = 650;
+
+alienblue.height = 180;
+alienblue.width = 170;
+
+
+stage.addChild(universe);
+stage.addChild(disco);
 stage.addChild(planet);
+stage.addChild(aliensmall);
+stage.addChild(alienbig);
+stage.addChild(alienblue);
+
 
 function animate() {
 
     requestAnimFrame( animate );
 
     // just for fun, lets rotate our alien a little
-    alien5.rotation += 0.1;
-    alien.rotation += 0.7;
-    planet.rotation += 0.025;
+    alienbig.rotation += 0.1;
+    aliensmall.rotation += 0.7;
+    planet.rotation += 0.010;
+    disco.rotation -= 0.03
+
+    alienblue.position.x += 10;
+    alienblue.position.y -= 4;
+
+    if (alienblue.position.x > 1400){
+        alienblue.position.x = 300
+        alienblue.position.y = 650
+    };
+
+    // disco.hitarea =
+
+    disco.mouseover = function(mouseData)
+    {
+        console.log('mouseover!')
+    };
+
+    graphics.mousemove = function(mouseData) {
+      console.log(mouseData);
+    }
+
+
 
     // render the stage
     renderer.render(stage);
